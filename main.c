edb3858fa7d3334ce98922be08c59d63acad320e
Henrique <hmcarrer@gmail.com>
2024-07-25 19:50:20 -0300

    printando tempo estimado de cada processo

--- main.c
+++ main.c
@@ -32,7 +32,6 @@ typedef struct {
     int num_dependencies; // Número de dependências
     sem_t sem; // Semáforo para controlar a execução do processo
     long exec_time; // Tempo de execução estimado
-    float real_exec_time; // Tempo de execução real
     char status;
     pid_t pid; // PID do processo
 } Process;
@@ -180,11 +179,11 @@ void execute_process(Process *process) {
     if (strcmp(process->command, "teste15") == 0) {
 
         long long i;
-        for (i = 0; i < 5000000000; i++);
+        for (i = 0; i < 8000000000; i++);
         printf("Processo %d finalizado (tempo de 15 segundos)\n", process->id);
     } else if (strcmp(process->command, "teste30") == 0) {
         long long i;
-        for (i = 0; i < 10000000000; i++);
+        for (i = 0; i < 16000000000; i++);
         printf("Processo %d finalizado (tempo de 30 segundos)\n", process->id);
         
     }
@@ -250,11 +249,6 @@ void sets_process_as_finished(Application *app, pid_t pid) {
     }
 }
 
-void save_real_exec_time(Application *app, int index, float time_diff) {
-    app->processes[index].real_exec_time = time_diff;
-    // printf("aqui aquino %f\n", app->processes[index].real_exec_time);
-}
-
 void execute_parallel(Application *app, int num_cores) {
     int active_processes = 0;
     int processes_remaining = app->num_processes;
@@ -268,19 +262,8 @@ void execute_parallel(Application *app, int num_cores) {
 
             pid_t pid = fork();
             if (pid == 0) {
-                // if(processes_remaining == app->num_processes) {
-                //     // TODO: init makespan
-                //     printf("Comecando a medir o makespan aqui\n");
-                // }
                 // Processo filho executa a tarefa
-                struct timeval start, end;
-                gettimeofday(&start, NULL);
                 execute_process(&app->processes[next_process]);
-                gettimeofday(&end, NULL);
-                printf("porra %f\n", time_diff(&start, &end));
-                // app->processes[next_process].real_exec_time = time_diff(&start, &end);
-                save_real_exec_time(app, next_process, time_diff(&start, &end));
-                printf("aqui aquino %f\n", app->processes[next_process].real_exec_time);
                 exit(0);
             } else if (pid > 0) {
                 // Processo pai continua
@@ -340,7 +323,7 @@ int main(int argc, char *argv[]) {
     float makespan = time_diff(&start, &end);
     printf("Makespan total: %0.2f segundos\n", makespan);
     for (int i = 0; i < app.num_processes; i++) {
-        printf("Tempo de execucao do processo %d: %0.2f segundos\n", app.processes[i].id, app.processes[i].real_exec_time);
+        printf("Tempo estimado de execucao do processo %d: %ld segundos\n", app.processes[i].id, app.processes[i].exec_time);
     }
 
     // Libere memória alocada
